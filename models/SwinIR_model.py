532abb7a94401b78c58438ae1097b3cfec83bc77
xia <3415671317@qq.com>
2025-09-13 21:32:59 +1000

    first

--- models/SwinIR_model.py
+++ models/SwinIR_model.py
@@ -700,11 +700,11 @@ class SwinIR(nn.Module):
     """
 
     def __init__(self, img_size=128, patch_size=1, in_chans=3,
-                 embed_dim=96, depths=[6, 6, 6, 6], num_heads=[6, 6, 6, 6],
-                 window_size=7, mlp_ratio=4., qkv_bias=True, qk_scale=None,
+                 embed_dim=180, depths=[6, 6, 6, 6, 6, 6], num_heads=[6, 6, 6, 6, 6, 6],
+                 window_size=8, mlp_ratio=2, qkv_bias=True, qk_scale=None,
                  drop_rate=0., attn_drop_rate=0., drop_path_rate=0.1,
                  norm_layer=nn.LayerNorm, ape=False, patch_norm=True,
-                 use_checkpoint=False, upscale=2, img_range=1., upsampler='', resi_connection='1conv',
+                 use_checkpoint=False, upscale=1, img_range=1.0, upsampler=None, resi_connection='1conv',
                  **kwargs):
         super(SwinIR, self).__init__()
         num_in_ch = in_chans
